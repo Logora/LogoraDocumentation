269147cbf4c794bbc624a67e0a7c6538d9c473d2
hboisgibault <h.boisgibault@gmail.com>
2021-01-25 15:19:30 +0100

    Site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://master.d24s2qxlp9xzpm.amplifyapp.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/app', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'app',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...

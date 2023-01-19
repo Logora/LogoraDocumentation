d5ceb44fe286c0bf4cbc43ef54dacae2d60d0a06
Henry Boisgibault <h.boisgibault@gmail.com>
2023-01-19 23:25:57 +0100

    Updated docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Henry\\Documents\\Projets\\Logora\\LogoraDocumentation\\website\\src\\css\\custom.css"),
-];
